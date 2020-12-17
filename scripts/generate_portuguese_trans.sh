960d80d57364478201cf8d4d491bbcce8b7125b4
Joao Paulo Reis Alvarenga <joaopaulo@stilingue.com.br>
2020-12-17 15:28:09 -0300

    Add multilingual librispeech

--- scripts/generate_portuguese_trans.sh
+++ scripts/generate_portuguese_trans.sh
@@ -3,9 +3,9 @@ python3 scripts/create_portuguese_trans.py --random_seed 42\
   --alcaim_path /home/joao/mestrado/datasets/alcaim \
   --lapsbm_val_path /home/joao/mestrado/datasets/LapsBM-val \
   --sid_path /home/joao/mestrado/datasets/sid \
-  --voxforge_path /home/joao/mestrado/datasets/voxforge \
   --common_voice_path /home/joao/mestrado/datasets/cv-corpus-5.1-2020-06-22/pt \
   --coral_path /home/joao/mestrado/datasets/c-oral/output \
+  --mls_path /home/joao/mestrado/datasets/mls_portuguese \
   --output_train ../datasets/train_$1.tsv \
   --output_test=../datasets/test_$1.tsv \
   --output_eval=../datasets/eval_$1.tsv
